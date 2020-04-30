d4d7e9c165dbf08ac2957773bc007435cf29a1eb
MayaSasson <mayokit22@gmail.com>
2020-05-01 00:25:42 +0300

    typeo

--- src/utils/errorMessages.ts
+++ src/utils/errorMessages.ts
@@ -1,4 +1,4 @@
 export default {};
 
-export const worngAuthMessage = 'אוף! שם המשתמש ו/או הסיסמא לא נכונים. פנה למנהל המערכת כדי לשחזר אותם';
+export const wrongAuthMessage = 'אוף! שם המשתמש ו/או הסיסמא לא נכונים. פנה למנהל המערכת כדי לשחזר אותם';
 export const dbErrorMessage = 'אופס! יש תקלה במערכת, נסה שוב מאוחר יותר';
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -83,7 +83,7 @@ export default class Login extends Vue {
           }
           this.$router.replace('/');
         } else {
-          this.errorMessage = errorMessages.worngAuthMessage;
+          this.errorMessage = errorMessages.wrongAuthMessage;
         }
       }
     })
