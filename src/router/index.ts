22f91044be45db4c8e7c45a012460d920a108c20
MayaSasson <62448339+MayaSasson@users.noreply.github.com>
2020-05-05 23:14:24 +0300

    feat(Login): Created the login page and implemented the login itself (#35)
    * now the login form is styled as supposed to
    
    * now the login works
    
    * changed the login ui
    
    * added the error message, styled the page and made the router work
    
    * don't redirect to login when logged
    
    * removed import
    
    * now the routing works
    
    * removed from comment
    
    * removed disabe
    
    * removed imports
    
    * the user needs only id for now
    
    * removed checking view
    
    * changed the way of work
    
    * COMMIT
    
    * Revert "removed checking view"
    
    This reverts commit 28d7d99cf8cecf7091ea4108d854636df8545161.
    
    * changed
    
    * Revert "changed the way of work"
    
    This reverts commit f6b7a90047dc28701351567cbbc47ce4f296362a.
    
    * checked if data is undefined
    
    * made few of the code review's changes
    
    * fixed more of cr comments
    
    * changed
    
    * removed unnecessary ifs
    
    * changed the styling

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,47 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Calendar from '../views/Calendar.vue';
-import Downloads from '../views/Downloads.vue';
+import { getModule } from 'vuex-module-decorators';
+import VueRouter, { RouteConfig } from 'vue-router';
+
+import StoreModule, { defaultUser } from '@/store/storeModule';
+import store from '@/store';
+import Login from '@/views/Login.vue';
+import Calendar from '@/views/Calendar.vue';
+import Downloads from '@/views/Downloads.vue';
 import FoodOrder from '../views/FoodOrder.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
+const storeModule = getModule(StoreModule, store);
+
+const routes: RouteConfig[] = [
   {
     path: '/',
     redirect: '/calendar',
   },
   {
+    path: '/login',
+    component: Login,
+  },
+  {
     path: '/calendar',
     component: Calendar,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/downloads',
     component: Downloads,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/orderFood',
     component: FoodOrder,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
@@ -29,6 +49,16 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.name === null) {
+    next('/');
+  } else if (to.meta.requiresAuth && storeModule.user === defaultUser) {
+    next('/login');
+  } else {
+    next();
+  }
 });
 
 export default router;
